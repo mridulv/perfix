83a372b70fe34ba7f8c445985b5163608305225d
Mridul Verma <mridulv09@gmail.com>
2024-01-02 22:09:03 +0530

    Adding Serialization / Deserialization for Certain Case Classes

--- app/io/perfix/model/DataType.scala
+++ app/io/perfix/model/DataType.scala
@@ -1,8 +1,32 @@
 package io.perfix.model
 
+import play.api.libs.json.{Format, JsError, JsString, JsSuccess, Reads, Writes}
+
 trait DataType
 
 case object BooleanType extends DataType
 case object StringType extends DataType
 case object DoubleType extends DataType
 case object IntType extends DataType
+
+object DataType {
+  // Define a custom Reads for deserialization
+  implicit val dataTypeReads: Reads[DataType] = Reads {
+    case JsString("BooleanType") => JsSuccess(BooleanType)
+    case JsString("StringType") => JsSuccess(StringType)
+    case JsString("DoubleType") => JsSuccess(DoubleType)
+    case JsString("IntType") => JsSuccess(IntType)
+    case _ => JsError("Invalid DataType")
+  }
+
+  // Define a custom Writes for serialization
+  implicit val dataTypeWrites: Writes[DataType] = Writes {
+    case BooleanType => JsString("BooleanType")
+    case StringType => JsString("StringType")
+    case DoubleType => JsString("DoubleType")
+    case IntType => JsString("IntType")
+  }
+
+  // Define a Format that combines Reads and Writes
+  implicit val dataTypeFormat: Format[DataType] = Format(dataTypeReads, dataTypeWrites)
+}
--- app/io/perfix/model/PerfixQuestionAnswer.scala
+++ app/io/perfix/model/PerfixQuestionAnswer.scala
@@ -1,11 +1,25 @@
 package io.perfix.model
 
 import io.perfix.question.Question.QuestionLabel
-import play.api.libs.json.{Json, Reads, Writes}
+import play.api.libs.json.{JsError, JsNumber, JsString, JsSuccess, Json, Reads, Writes}
 
 case class PerfixQuestionAnswer(questionLabel: QuestionLabel, answer: Any)
 
 object PerfixQuestionAnswer {
+  // Custom Reads for deserializing Any as a string or an integer
+  implicit val anyReads: Reads[Any] = Reads {
+    case JsString(str) => JsSuccess(str)
+    case JsNumber(num) if num.isValidInt => JsSuccess(num.toInt)
+    case _ => JsError("Invalid Any type")
+  }
+
+  // Custom Writes for serializing Any
+  implicit val anyWrites: Writes[Any] = Writes {
+    case str: String => JsString(str)
+    case int: Int => JsNumber(int)
+    case _ => throw new UnsupportedOperationException("Serialization of this Any type is not supported")
+  }
+
   implicit val PerfixQuestionAnswerWrites: Writes[PerfixQuestionAnswer] = Json.writes[PerfixQuestionAnswer]
   implicit val PerfixQuestionAnswerReads: Reads[PerfixQuestionAnswer] = Json.reads[PerfixQuestionAnswer]
 }
