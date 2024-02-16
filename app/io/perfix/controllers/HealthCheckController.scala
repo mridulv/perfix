9d3caaa7947543aa382ab3bcaa01f851314047c6
Mridul Verma <mridulv09@gmail.com>
2024-02-16 14:17:17 +0530

    Adding HealthCheckController

---
+++ app/io/perfix/controllers/HealthCheckController.scala
@@ -0,0 +1,12 @@
+package io.perfix.controllers
+
+import com.google.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+
+@Singleton
+class HealthCheckController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  def health: Action[AnyContent] = Action {
+    Ok("Healthy")
+  }
+}
