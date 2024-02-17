5120f7f71492165b7727e802e4386ae35447d61d
Mridul Verma <mridulv09@gmail.com>
2024-02-18 04:27:38 +0530

    Adding Flag for usingInstanceRole

--- app/io/perfix/launch/AWSCloudParams.scala
+++ app/io/perfix/launch/AWSCloudParams.scala
@@ -7,5 +7,6 @@ class AWSCloudParams extends QuestionParams {
   var accessKey: Option[String] = None
   var accessSecret: Option[String] = None
   var launchDB: Boolean = false
+  var useInstanceRole: Boolean = false
 
 }
