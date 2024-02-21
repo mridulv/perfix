a6c16ed1f6a91b0d023fe924a13ad67baaa6803e
Mridul Verma <mridulv09@gmail.com>
2024-02-22 02:42:14 +0530

    Changing the name from EKSConfig

--- app/io/perfix/common/CommonConfig.scala
+++ app/io/perfix/common/CommonConfig.scala
@@ -1,8 +1,9 @@
 package io.perfix.common
 
-object EksConfig {
+object CommonConfig {
 
   val EKS_NAME = "test-cluster"
   val EKS_REGION = "us-west-2"
+  val DB_SUBNET_GROUP_NAME = "DBSubnet"
 
 }
