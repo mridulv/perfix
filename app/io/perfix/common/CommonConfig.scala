89f33de68c86964b2172c67e6b4d1d4c989eb291
Mridul Verma <mridulv09@gmail.com>
2024-02-23 01:09:26 +0530

    Adding an Env Variable for Mode to check if it is a Trial mode

--- app/io/perfix/common/CommonConfig.scala
+++ app/io/perfix/common/CommonConfig.scala
@@ -5,5 +5,9 @@ object CommonConfig {
   val EKS_NAME = "test-cluster"
   val EKS_REGION = "us-west-2"
   val DB_SUBNET_GROUP_NAME = "DBSubnet"
+  val IS_TRIAL_MODE: Boolean = Option(System.getenv("MODE")).map(_.toLowerCase()) match {
+    case Some("trial") => true
+    case _ => false
+  }
 
 }
