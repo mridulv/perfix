d84b72eb9fb60d7a3594e10c90b900ef8a03e63b
Mridul Verma <mridulv09@gmail.com>
2024-02-24 16:05:06 +0530

    Adding Env Variables for EKS Config and Network properties

--- app/io/perfix/common/CommonConfig.scala
+++ app/io/perfix/common/CommonConfig.scala
@@ -2,9 +2,9 @@ package io.perfix.common
 
 object CommonConfig {
 
-  val EKS_NAME = "test-cluster"
-  val EKS_REGION = "us-west-2"
-  val DB_SUBNET_GROUP_NAME = "DBSubnet"
+  val EKS_NAME: String = Option(System.getenv("EKS_NAME")).map(_.toLowerCase()).getOrElse("test-cluster")
+  val EKS_REGION: String = Option(System.getenv("EKS_REGION")).map(_.toLowerCase()).getOrElse("us-west-2")
+  val DB_SUBNET_GROUP_NAME: String = Option(System.getenv("DB_SUBNET_GROUP_NAME")).map(_.toLowerCase()).getOrElse("DBSubnet")
   val IS_TRIAL_MODE: Boolean = Option(System.getenv("MODE")).map(_.toLowerCase()) match {
     case Some("trial") => true
     case _ => false
