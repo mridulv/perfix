eecbf4f26b9054d52fa81a15a59c3016ca3d2f3f
Mridul Verma <mridulv09@gmail.com>
2024-03-04 21:55:04 +0530

    Refactoring Controller and Routes

---
+++ app/io/perfix/controllers/PerfixQuestionnaireController.scala
@@ -0,0 +1,34 @@
+package io.perfix.controllers
+
+import controllers.AssetsFinder
+import io.perfix.common.PerfixManager
+import io.perfix.model.PerfixQuestionAnswers
+import play.api.libs.json.Json
+import play.api.mvc.{AbstractController, ControllerComponents, Results}
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class PerfixQuestionnaireController @Inject()(cc: ControllerComponents,
+                                              perfixManager: PerfixManager)(implicit assetsFinder: AssetsFinder)
+  extends AbstractController(cc) {
+
+  def questionnaire(storeName: String) = Action {
+    Results.Ok(Json.toJson(perfixManager.startQuestionnaire(storeName)))
+  }
+
+  def questions(experimentId: Int) = Action {
+    Results.Ok(Json.toJson(perfixManager.nextQuestion(experimentId)))
+  }
+
+  def submitQuestion(experimentId: Int) = Action(parse.json) { request =>
+    val perfixQuestionAnswers = request.body.as[PerfixQuestionAnswers]
+    perfixManager.submitQuestionAnswer(experimentId, perfixQuestionAnswers)
+    Results.Ok
+  }
+
+  def startExperiment(experimentId: Int) = Action {
+    Results.Ok(Json.toJson(perfixManager.startExperiment(experimentId)))
+  }
+
+}
