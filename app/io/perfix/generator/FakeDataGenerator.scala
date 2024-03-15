f8fbdda1acd0e210b91ad72b71f9824544f01c5c
Mridul Verma <mridulv09@gmail.com>
2024-03-15 23:47:10 +0530

    Adding Functionality for generating specific values with a particular distribution

--- app/io/perfix/generator/FakeDataGenerator.scala
+++ app/io/perfix/generator/FakeDataGenerator.scala
@@ -3,6 +3,8 @@ package io.perfix.generator
 import com.github.javafaker.Faker
 import io.perfix.model.{ColumnType, DataDescription, DataWithDescription}
 
+import scala.util.Random
+
 class FakeDataGenerator extends DataGenerator {
   val faker = new Faker
 
@@ -19,11 +21,19 @@ class FakeDataGenerator extends DataGenerator {
         val columnName = column.columnName
         val columnType = column.columnType
         var value = columnType.getValue
+        val randomNum = Random.nextInt(10000) + 1
         if (columnType.isUnique) {
           while (uniqueValuesByColumn.getOrElse(columnName, Set.empty).contains(value)) {
             value = columnType.getValue
           }
           uniqueValuesByColumn.update(columnName, uniqueValuesByColumn.getOrElse(columnName, Set.empty) + value)
+        }
+        column.columnValueDistribution match {
+          case Some(valueProbability) =>
+            if (randomNum < valueProbability.probability * 10000) {
+              value = valueProbability.value
+            }
+          case _ => ()
         }
         row += (columnName -> value)
       }
