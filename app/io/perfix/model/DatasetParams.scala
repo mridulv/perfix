56eb97737ecd84622bb6f2d3e3b610d0ce3ab420
Mridul Verma <mridulv09@gmail.com>
2024-04-08 22:23:40 +0530

    Fixing compilation issues

--- app/io/perfix/model/DatasetParams.scala
+++ app/io/perfix/model/DatasetParams.scala
@@ -2,8 +2,8 @@ package io.perfix.model
 
 import play.api.libs.json.{Format, Json}
 
-class DatasetParams(rows: Int, columns: Seq[ColumnDescription])
+case class DatasetParams(rows: Int, columns: Seq[ColumnDescription])
 
 object DatasetParams {
-  implicit val DatasetParams: Format[DatasetParams] = Json.format[DatasetParams]
+  implicit val DatasetParamsFormatter: Format[DatasetParams] = Json.format[DatasetParams]
 }
