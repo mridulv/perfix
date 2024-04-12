13dce65f3460134ee4ed043645d87b8b75c74207
Mridul Verma <mridulv09@gmail.com>
2024-04-13 00:05:12 +0530

    Implementing wireframe for databaseconfigmanager

--- app/io/perfix/manager/DatabaseConfigManager.scala
+++ app/io/perfix/manager/DatabaseConfigManager.scala
@@ -1,15 +1,30 @@
 package io.perfix.manager
 
 import com.google.inject.Singleton
-import io.perfix.model.{DatabaseConfigId, DatabaseConfigParams}
+import io.perfix.model.{DatabaseConfigId, DatabaseConfigParams, DatasetId, DatasetParams}
+
+import scala.collection.mutable
+import scala.util.Random
 
 @Singleton
 class DatabaseConfigManager {
+  private val mapping: mutable.Map[DatabaseConfigId, DatabaseConfigParams] = mutable.Map.empty[DatabaseConfigId, DatabaseConfigParams]
 
-  def create(databaseConfigParams: DatabaseConfigParams): DatabaseConfigId = ???
+  def create(databaseConfigParams: DatabaseConfigParams): DatabaseConfigId = {
+    val id: Int = Random.nextInt()
+    mapping.put(
+      DatabaseConfigId(id),
+      databaseConfigParams.copy(databaseConfigId = Some(DatabaseConfigId(id)))
+    )
+    DatabaseConfigId(id)
+  }
 
-  def get(databaseConfigId: DatabaseConfigId): DatabaseConfigParams = ???
+  def get(databaseConfigId: DatabaseConfigId): DatabaseConfigParams = {
+    mapping(databaseConfigId)
+  }
 
-  def all(): Seq[DatabaseConfigParams] = ???
+  def all(): Seq[DatabaseConfigParams] = {
+    mapping.values.toSeq
+  }
 
 }
