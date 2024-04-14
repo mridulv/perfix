357ce475de15a60fc95e65dabacdcb6759495a0b
ahmedullah12 <gulamjoy321@gmail.com>
2024-04-14 09:19:32 +0600

    Post dataset modal and api added

--- ui/src/Layout/DashboardLayout.js
+++ ui/src/Layout/DashboardLayout.js
@@ -1,84 +1,123 @@
-import React, { useState } from 'react';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { MdOutlineDashboard } from "react-icons/md";
 import { FaUserAlt } from "react-icons/fa";
 import { IoReturnUpBack } from "react-icons/io5";
 import { FaAngleLeft } from "react-icons/fa6";
 
 const DashboardLayout = () => {
-    const menus = [
-        {
-            name: "Dashboard",
-            link: "/",
-            icon: <MdOutlineDashboard color='white' size={30}/>
-        },
-        
-        {
-            name: "DB Configuration",
-            link: "/db-configuration",
-            icon: <MdOutlineDashboard color='white' size={30}/>
-        },
-        {
-            name: "Datasets",
-            link: "/datasets",
-            icon: <MdOutlineDashboard color='white' size={30}/>
-        },
-        {
-            name: "Experiment",
-            link: "/experiment",
-            icon: <MdOutlineDashboard color='white' size={30}/>
-        },
-    ];
-    const [menuOpen, setMenuOpen] = useState(true);
-    const location = useLocation();
+  const menus = [
+    {
+      name: "Dashboard",
+      link: "/",
+      icon: <MdOutlineDashboard color="white" size={30} />,
+    },
 
-    const userProfile = {
-        name: "John Doe",
-        profilePic: <FaUserAlt />
-    };
+    {
+      name: "DB Configuration",
+      link: "/db-configuration",
+      icon: <MdOutlineDashboard color="white" size={30} />,
+    },
+    {
+      name: "Datasets",
+      link: "/datasets",
+      icon: <MdOutlineDashboard color="white" size={30} />,
+    },
+    {
+      name: "Experiment",
+      link: "/experiment",
+      icon: <MdOutlineDashboard color="white" size={30} />,
+    },
+  ];
+  const [menuOpen, setMenuOpen] = useState(true);
+  const location = useLocation();
 
-    
-    return (
-        <div className='flex'> 
-            <div className={`${menuOpen ? "w-72" : "w-20"} duration-300 h-screen flex flex-col justify-between  bg-gray-400 px-4 py-3 relative`}>
-                <button className={`bg-blue-500 rounded-full px-2 py-2 cursor-pointer absolute top-9 -right-3 ${!menuOpen && "rotate-180"}`} onClick={() => setMenuOpen(!menuOpen)}>
-                    <FaAngleLeft  color='white'/>
-                </button>
-                <div className=''>
-                    <div className='mb-5'>
-                        <h1 className={`italic text-2xl font-bold text-white ${!menuOpen && "scale-0"}`}>Dashboard</h1>
-                    </div>
-                    <div>
-                        {
-                            menus.map(menu => (
-                                <Link key={menu.name} className={`font-bold hover:bg-purple-300 ${location.pathname === menu.link ? "bg-purple-600" : ""}  ${menuOpen ? "w-52" : "w-12"} px-3 py-3 rounded-lg flex gap-4 items-center`} to={`${menu.link}`}>
-                                    {menu.icon}
-                                   <span className={`${!menuOpen && "hidden"} origin-left duration-200`}> {menu.name}</span>
-                                </Link>
-                            ))
-                        }
-                    </div>
-                </div>
-                <div className="flex flex-col ps-2 mb-2 border-t border-gray-600 py-5">
-                    <div className='flex items-center mb-4'>
-                        <span>{userProfile.profilePic}</span>
-                        <span className={`ml-3 text-white ${!menuOpen && "hidden"} origin-left duration-200`}>
-                            {userProfile.name}
-                        </span>
-                    </div>
-                    <div className='mt-2'>
-                        <button className={`bg-purple-500 text-white ${menuOpen ? "px-5" : "px-2"} h-[40px] py-2 flex items-center gap-3 rounded-xl origin-left duration-500`}>
-                            <span className={`${!menuOpen && "hidden"} `}>Back to Home</span>
-                        <IoReturnUpBack size={20} color='white'/>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div className='flex-1 h-screen'>
-                <Outlet></Outlet>
-            </div>
+  const userProfile = {
+    name: "John Doe",
+    profilePic: <FaUserAlt />,
+  };
+
+  return (
+    <div className="flex">
+      
+      {/* sidebar */}
+      <div
+        className={`${
+          menuOpen ? "w-72" : "w-20"
+        } duration-300 h-screen flex flex-col justify-between  bg-gray-400 px-4 py-3 relative`}
+      >
+        <button
+          className={`bg-blue-500 hidden md:block rounded-full px-2 py-2 cursor-pointer absolute top-9 -right-3 ${
+            !menuOpen && "rotate-180"
+          }`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <FaAngleLeft color="white" />
+        </button>
+        <div className="">
+          <div className="mb-5">
+            <h1
+              className={`italic text-2xl font-bold text-white ${
+                !menuOpen && "scale-0"
+              }`}
+            >
+              Dashboard
+            </h1>
+          </div>
+          <div>
+            {menus.map((menu) => (
+              <Link
+                key={menu.name}
+                className={`font-bold hover:bg-purple-300 ${
+                  location.pathname === menu.link ? "bg-purple-600" : ""
+                }  ${
+                  menuOpen ? "w-52" : "w-12"
+                } px-3 py-3 rounded-lg flex gap-4 items-center`}
+                to={`${menu.link}`}
+              >
+                {menu.icon}
+                <span
+                  className={`${
+                    !menuOpen && "hidden"
+                  } origin-left duration-200`}
+                >
+                  {" "}
+                  {menu.name}
+                </span>
+              </Link>
+            ))}
+          </div>
         </div>
-    );
+        <div className="flex flex-col ps-2 mb-2 border-t border-gray-600 py-5">
+          <div className="flex items-center mb-4">
+            <span>{userProfile.profilePic}</span>
+            <span
+              className={`ml-3 text-white ${
+                !menuOpen && "hidden"
+              } origin-left duration-200`}
+            >
+              {userProfile.name}
+            </span>
+          </div>
+          <div className="mt-2">
+            <button
+              className={`bg-purple-500 text-white ${
+                menuOpen ? "px-5" : "px-2"
+              } h-[40px] py-2 flex items-center gap-3 rounded-xl origin-left duration-500`}
+            >
+              <span className={`${!menuOpen && "hidden"} `}>Back to Home</span>
+              <IoReturnUpBack size={20} color="white" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* contents */}
+      <div className="flex-1 h-screen">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
--- ui/src/Pages/Dashboard/Datasets/Datasets.js
+++ ui/src/Pages/Dashboard/Datasets/Datasets.js
@@ -1,59 +1,113 @@
-import React, { useEffect, useState } from 'react';
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import AddDatasetModal from "../../../components/AddDatasetModal";
+import { BiTrash } from "react-icons/bi";
+import { FaPlus } from "react-icons/fa6";
+import { useQuery } from "react-query";
+
 
 const Datasets = () => {
+  const [datasets, setDatasets] = useState({});
+  const [open, setOpen] = useState(false);
 
-    const [datasets, setDatasets] = useState({});
+  // useEffect(() => {
+  //   const fetchDatasets = async () => {
+  //     const res = await axios.get("http://localhost:9000/dataset");
+  //     const data = await res.data;
+  //     console.log(data);
+  //   };
 
-    useEffect(() => {
-        const fetchDatasets = async() => {
-            const res = await axios.get("http://localhost:9000/dataset");
-            const data = await res.data;
-            console.log(data);
-        }
+  //   fetchDatasets();
+  // }, []);
 
-        fetchDatasets();
-    }, [])
+  const {data: datasets1, isLoading, refetch} = useQuery({
+    queryKey: ["datasets"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:9000/dataset");
+      const data = await res.data;
+      console.log(data);
+      return data;
+    },
+  });
 
-    const handleSubmit = async(event) => {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        const number = form.number.value;
-        const desc = form.description.value;
-        console.log(name, number, desc);
+  console.log(datasets1);
 
-        try{
-            const url = "";
-            const data = {};
-            const response = await axios.post(url, data);
-            console.log(response.data);
-        }
-        catch(err){
-            console.log(err);
-        }
+  const handleAddDataset = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const columnName = form.columnName.value;
+    const columnType = form.columnType.value;
+    console.log(columnName, columnType);
+
+    try {
+      const url = "http://localhost:9000/dataset";
+      const data = {
+        columnName,
+        columnType,
+      };
+      const response = await axios.post(url, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response.data);
+      if(response.status === 200){
+        refetch();
+      }
+    } catch (err) {
+      console.log(err);
     }
-    return (
-        <div className='h-full flex justify-center items-center'>
-            <form  className='w-[300px] ' onSubmit={handleSubmit}>
-                <div className='flex flex-col my-4'>
-                    <label htmlFor="">Dataset Name: </label>
-                    <input className='border-2 border-gray-500 px-2 py-2 rounded mt-2' placeholder='Name' type="text" name="name" id="" required/>
-                </div>
-                <div className='flex flex-col my-4'>
-                    <label htmlFor="">Number of Columns: </label>
-                    <input className='border-2 border-gray-500 px-2 py-2 rounded mt-2' placeholder='Number' type="number" name="number" id="" min={1} max={10} required/>
-                </div>
-                <div className='flex flex-col my-4'>
-                    <label htmlFor="">Dataset Description: </label>
-                    <textarea  className='border-2 border-gray-500 px-2 py-2 rounded mt-2 resize-none' name="description" id="" cols="30" rows="10" required></textarea>
-                </div>
-                <div className='flex justify-center'>
-                    <button className='bg-purple-400 text-white px-4 py-2 rounded-md'>Submit</button>
-                </div>
-            </form>
+  };
+
+  if(isLoading) return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+            <span className="loading loading-spinner loading-lg"></span>
         </div>
-    );
+  )
+  return (
+    <div className="px-3 py-4">
+      <h2 className="text-2xl text-center font-bold text-orange-600 my-3">Datasets</h2>
+      <div className="flex justify-end my-4 mx-8">
+        <button className="btn btn-error text-white" onClick={() => setOpen(true)}>
+          <FaPlus/> Add
+        </button>
+      </div>
+      <AddDatasetModal open={open} onClose={() => setOpen(false)}>
+        <div className=" w-[80%] md:w-[400px]">
+          <div className="mx-auto my-4 w-[80%] md:w-[300px]">
+            <h3 className="text-lg my-4 font-bold text-center">Add Dataset</h3>
+            <form onSubmit={handleAddDataset}>
+              <div className="flex flex-col my-4">
+                <label htmlFor="">Column Name: </label>
+                <input
+                  className="border-2 border-gray-500 px-2 py-2 rounded mt-2"
+                  placeholder="Name"
+                  type="text"
+                  name="columnName"
+                  id=""
+                  required
+                />
+              </div>
+              <div className="flex flex-col my-4">
+                <label htmlFor="">Column Type:</label>
+                <select
+                  id="column-type"
+                  name="columnType"
+                  className="block w-full mt-2 px-4 py-2 pr-8 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-gray-500"
+                >
+                  <option value="NameType">NameType</option>
+                  <option value="AddressType">AddressType</option>
+                </select>
+              </div>
+              <div className="flex justify-center">
+              <input type="submit" className="btn btn-primary text-center" value={"Submit"}/>
+              </div>
+            </form>
+          </div>
+        </div>
+      </AddDatasetModal>
+    </div>
+  );
 };
 
 export default Datasets;
