ca6d7ff79cca0f00dc758a7606d94afeba9c1b80
Mridul Verma <mridulv09@gmail.com>
2024-05-21 19:04:06 +0530

    Integrating Secure API Endpoint with each existing API

--- app/io/perfix/controllers/DatabaseConfigController.scala
+++ app/io/perfix/controllers/DatabaseConfigController.scala
@@ -5,40 +5,52 @@ import io.perfix.manager.DatabaseConfigManager
 import io.perfix.model.{DatabaseConfigId, DatabaseConfigParams, EntityFilter}
 import io.perfix.model.DatabaseConfigId._
 import io.perfix.model.DatabaseConfigParams._
+import org.pac4j.core.profile.UserProfile
+import org.pac4j.play.scala.{Security, SecurityComponents}
 import play.api.libs.json.Json
 import play.api.mvc.{BaseController, ControllerComponents, Results}
 
 @Singleton
-class DatabaseConfigController @Inject()(val controllerComponents: ControllerComponents,
-                                         databaseConfigManager: DatabaseConfigManager) extends BaseController {
-  def create = Action(parse.json) { request =>
-    val databaseConfigParams = request.body.as[DatabaseConfigParams]
-    Results.Ok(Json.toJson(databaseConfigManager.create(databaseConfigParams)))
+class DatabaseConfigController @Inject()(val controllerComponents: SecurityComponents,
+                                         databaseConfigManager: DatabaseConfigManager) extends Security[UserProfile] {
+  def create = Secure("Google2Client") {
+    Action(parse.json) { request =>
+      val databaseConfigParams = request.body.as[DatabaseConfigParams]
+      Results.Ok(Json.toJson(databaseConfigManager.create(databaseConfigParams)))
+    }
   }
 
-  def get(databaseConfigId: Int) = Action { request =>
-    Results.Ok(Json.toJson(databaseConfigManager.get(DatabaseConfigId(databaseConfigId))))
+  def get(databaseConfigId: Int) = Secure("Google2Client") {
+    Action { request =>
+      Results.Ok(Json.toJson(databaseConfigManager.get(DatabaseConfigId(databaseConfigId))))
+    }
   }
 
-  def update(databaseConfigId: Int) = Action(parse.json) { request =>
-    val databaseConfigParams = request.body.as[DatabaseConfigParams]
-    Results.Ok(
-      Json.toJson(
-        databaseConfigManager.update(
-          DatabaseConfigId(databaseConfigId),
-          databaseConfigParams
+  def update(databaseConfigId: Int) = Secure("Google2Client"){
+    Action(parse.json) { request =>
+      val databaseConfigParams = request.body.as[DatabaseConfigParams]
+      Results.Ok(
+        Json.toJson(
+          databaseConfigManager.update(
+            DatabaseConfigId(databaseConfigId),
+            databaseConfigParams
+          )
         )
       )
-    )
+    }
   }
 
-  def all = Action(parse.json) { request =>
-    val filters = request.body.as[Seq[EntityFilter]]
-    Results.Ok(Json.toJson(databaseConfigManager.all(filters)))
+  def all = Secure("Google2Client") {
+    Action(parse.json) { request =>
+      val filters = request.body.as[Seq[EntityFilter]]
+      Results.Ok(Json.toJson(databaseConfigManager.all(filters)))
+    }
   }
 
-  def delete(databaseConfigId: Int) = Action { request =>
-    databaseConfigManager.delete(DatabaseConfigId(databaseConfigId))
-    Results.Ok
+  def delete(databaseConfigId: Int) = Secure("Google2Client") {
+    Action { request =>
+      databaseConfigManager.delete(DatabaseConfigId(databaseConfigId))
+      Results.Ok
+    }
   }
 }
