7e72e03900ba14cc033a6d8a90e66c274b997cdf
Mridul Verma <mridulv09@gmail.com>
2024-05-24 23:10:20 +0530

    Adding /me as callback endpoint in logout + callback for auth

--- app/io/perfix/auth/SecurityModule.scala
+++ app/io/perfix/auth/SecurityModule.scala
@@ -26,12 +26,12 @@ class SecurityModule(environment: Environment, configuration: Configuration) ext
     bind(classOf[SecurityComponents]).to(classOf[DefaultSecurityComponents])
 
     val callbackController = new CallbackController()
-    callbackController.setDefaultUrl("/")
+    callbackController.setDefaultUrl("/me")
     bind(classOf[CallbackController]).toInstance(callbackController)
 
     // logout
     val logoutController = new LogoutController()
-    logoutController.setDefaultUrl("/")
+    logoutController.setDefaultUrl("/me")
     bind(classOf[LogoutController]).toInstance(logoutController)
   }
 
