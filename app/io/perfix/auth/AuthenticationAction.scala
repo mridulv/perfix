e7bf173fb2682dfe2e4c7a5e02a9d46c40591a20
Mridul Verma <mridulv09@gmail.com>
2024-05-27 10:08:45 +0530

    Adding an Authenticated Action to be used in all of the API endpoints

---
+++ app/io/perfix/auth/AuthenticationAction.scala
@@ -0,0 +1,32 @@
+package io.perfix.auth
+
+import com.google.inject.Inject
+import io.perfix.model.UserInfo
+import org.pac4j.core.context.session.SessionStore
+import org.pac4j.core.profile.ProfileManager
+import org.pac4j.play.PlayWebContext
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc._
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.CollectionConverters._
+
+class AuthenticationAction @Inject() (parser: BodyParsers.Default,
+                                      sessionStore: SessionStore)(implicit ec: ExecutionContext)
+  extends ActionBuilderImpl(parser)
+    with Logging {
+  override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
+    val webContext = new PlayWebContext(request)
+    val profileManager = new ProfileManager(webContext, sessionStore)
+    val profiles = profileManager.getProfiles.asScala
+
+    if (profiles.nonEmpty) {
+      block(request)
+    } else {
+      Future.successful {
+        Results.Forbidden
+      }
+    }
+  }
+}
