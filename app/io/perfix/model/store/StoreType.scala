e4aec0d55a8248d1c652336f825d0a9ad71d31c0
Mridul Verma <mridulv09@gmail.com>
2024-06-06 22:14:25 +0530

    Fixing compilation Issue

--- app/io/perfix/model/store/StoreType.scala
+++ app/io/perfix/model/store/StoreType.scala
@@ -4,22 +4,22 @@ import play.api.libs.json._
 
 object StoreType extends Enumeration {
   type StoreType = Value
-  val MySQLStoreType, RedisStoreType, DynamoDBStoreType, MongoDBStoreType = Value
+  val MySQL, Redis, DynamoDB, MongoDB = Value
 
-  implicit val StoreTypeReads: Reads[StoreType] = Reads {
-    case JsString("MySQLStoreType") => JsSuccess(MySQLStoreType)
-    case JsString("RedisStoreType") => JsSuccess(RedisStoreType)
-    case JsString("DynamoDBStoreType") => JsSuccess(DynamoDBStoreType)
-    case JsString("MongoDBStoreType") => JsSuccess(MongoDBStoreType)
+  implicit val StoreTypeReads: Reads[StoreType.Value] = Reads {
+    case JsString("MySQL") => JsSuccess(MySQL)
+    case JsString("Redis") => JsSuccess(Redis)
+    case JsString("DynamoDB") => JsSuccess(DynamoDB)
+    case JsString("MongoDB") => JsSuccess(MongoDB)
     case _ => JsError("Invalid DataType")
   }
 
   // Define a custom Writes for serialization
-  implicit val StoreTypeWrites: Writes[StoreType] = Writes {
-    case MySQLStoreType => JsString("MySQLStoreType")
-    case RedisStoreType => JsString("RedisStoreType")
-    case DynamoDBStoreType => JsString("DynamoDBStoreType")
-    case MongoDBStoreType => JsString("MongoDBStoreType")
+  implicit val StoreTypeWrites: Writes[StoreType.Value] = Writes {
+    case MySQL => JsString("MySQL")
+    case Redis => JsString("Redis")
+    case DynamoDB => JsString("DynamoDB")
+    case MongoDB => JsString("MongoDB")
   }
 
   implicit val StoreTypeFormat: Format[StoreType] = Format(StoreTypeReads, StoreTypeWrites)
