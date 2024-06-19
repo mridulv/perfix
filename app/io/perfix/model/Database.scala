6171b7ce904500d856ff4ae39a6791a2f2e70577
Mridul Verma <mridulv09@gmail.com>
2024-06-19 10:41:01 +0530

    Adding more relevant params to Database

--- app/io/perfix/model/Database.scala
+++ app/io/perfix/model/Database.scala
@@ -1,9 +1,15 @@
 package io.perfix.model
 
 import io.perfix.model.DatabaseCategory.DatabaseCategory
-import io.perfix.model.api.DatabaseFormInput
+import io.perfix.model.api.{DatabaseConfigParams, DatabaseFormInput}
 import io.perfix.model.store.StoreType.StoreType
+import play.api.libs.json.{Format, Json}
 
 case class Database(name: StoreType,
                     databaseCategory: DatabaseCategory,
-                    databaseFormInput: DatabaseFormInput, database)
+                    databaseFormInput: DatabaseFormInput,
+                    databaseConfigParams: DatabaseConfigParams)
+
+object Database {
+  implicit val DatabaseFormatter: Format[Database] = Json.format[Database]
+}
