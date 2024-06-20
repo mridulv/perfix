cc0b48f2cc39d60d627d246bc3b458366a2d3c26
Mridul Verma <mridulv09@gmail.com>
2024-06-20 22:54:07 +0530

    Fixing few more experimentResult objects

--- app/io/perfix/model/experiment/MultipleExperimentResult.scala
+++
@@ -1,9 +0,0 @@
-package io.perfix.model.experiment
-
-import play.api.libs.json.{Format, Json}
-
-case class MultipleExperimentResult(results: Seq[SingleExperimentResult])
-
-object MultipleExperimentResult {
-  implicit val MultipleExperimentResultFormatter: Format[MultipleExperimentResult] = Json.format[MultipleExperimentResult]
-}
