481b5a7b35fe2ab0633049eaa10b8d08648a03a2
Mridul Verma <mridulv09@gmail.com>
2024-07-04 22:55:45 +0530

    Integrating conversations with ChatGPT

--- app/io/perfix/controllers/ConversationController.scala
+++ app/io/perfix/controllers/ConversationController.scala
@@ -3,14 +3,15 @@ package io.perfix.controllers
 import io.perfix.auth.AuthenticationAction
 
 import javax.inject.{Inject, Singleton}
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsString, JsValue, Json}
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import io.perfix.manager.ConversationManager
-import io.perfix.model.api.{ConversationId, ConversationParams}
+import io.perfix.model.api.{ConversationId, ConversationMessage, ConversationParams}
 import org.pac4j.core.profile.UserProfile
 import org.pac4j.play.scala.{Security, SecurityComponents}
 
 import scala.concurrent.ExecutionContext
+import scala.util.parsing.json.JSONObject
 
 @Singleton
 class ConversationController @Inject()(val controllerComponents: SecurityComponents,
@@ -25,11 +26,17 @@ class ConversationController @Inject()(val controllerComponents: SecurityCompone
   }
 
   def get(id: Int): Action[AnyContent] = authenticationAction {
-    val conversation = conversationManager.get(ConversationId(id))
-    conversation match {
-      case Some(params) => Ok(Json.toJson(params))
-      case None => NotFound
+    try {
+      Ok(Json.toJson(conversationManager.get(ConversationId(id))))
+    } catch {
+      case e: Exception => NotFound
     }
+  }
+
+  def converse(id: Int): Action[JsValue] = authenticationAction(parse.json) { request =>
+    val message = (request.body \ "message").as[String]
+    val conversationResponse = conversationManager.converse(ConversationId(id), message)
+    Ok(Json.obj("message" -> conversationResponse))
   }
 
   def list: Action[AnyContent] = authenticationAction {
--- app/io/perfix/manager/ConversationManager.scala
+++ app/io/perfix/manager/ConversationManager.scala
@@ -1,12 +1,21 @@
 package io.perfix.manager
 
+import akka.actor.ActorSystem
+import akka.stream.Materializer
 import com.google.inject.{Inject, Singleton}
+import io.cequence.openaiscala.domain.ChatRole
+import io.cequence.openaiscala.service.OpenAIServiceFactory
 import io.perfix.db.ConversationStore
-import io.perfix.model.api.{ConversationId, ConversationParams}
+import io.perfix.model.api.{ConversationId, ConversationMessage, ConversationParams}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext}
 
 
 @Singleton
 class ConversationManager @Inject()(conversationStore: ConversationStore) {
+  implicit val ec = ExecutionContext.global
+  implicit val materializer = Materializer(ActorSystem())
 
   def create(conversationParams: ConversationParams): ConversationParams = {
     val createdConversationParams = conversationStore.create(conversationParams)
@@ -19,8 +28,24 @@ class ConversationManager @Inject()(conversationStore: ConversationStore) {
     updatedConversationParams
   }
 
-  def get(conversationId: ConversationId): Option[ConversationParams] = {
-    conversationStore.get(conversationId)
+  def get(conversationId: ConversationId): ConversationParams = {
+    conversationStore
+      .get(conversationId)
+      .getOrElse(new RuntimeException(s"Invalid ${conversationId}"))
+  }
+
+  def converse(conversationId: ConversationId, message: String): String = {
+    val conversationParams = get(conversationId)
+    val updatedConversationParams = conversationParams.addConversation(ConversationMessage(ChatRole.User, message))
+    val assistantMessage: String = updatedConversationParams.conversationDetails match {
+      case Some(conversation) => chatResponse(conversation.messages)
+      case None => throw new RuntimeException(s"Invalid ConversationParams for id: ${conversationId}")
+    }
+    conversationStore.update(
+      conversationId,
+      updatedConversationParams.addConversation(ConversationMessage(ChatRole.Assistant, assistantMessage))
+    )
+    assistantMessage
   }
 
   def list(): Seq[ConversationParams] = {
@@ -29,5 +54,19 @@ class ConversationManager @Inject()(conversationStore: ConversationStore) {
 
   def delete(conversationId: ConversationId): Int = {
     conversationStore.delete(conversationId)
+  }
+
+  private def chatResponse(messages: Seq[ConversationMessage]): String = {
+    val systemMessage = ConversationMessage(
+      ChatRole.System,
+      "Assist users with database performance benchmarking. It guides users in defining their use-case, understanding their data schema, and choosing the best database for their needs. It probes users about their data schema, column types, and specific queries they wish to run. It suggests the most suitable database, helps define their queries and workload in SQL, and assists in understanding if they want to run a benchmark against different databases. If users choose to run an experiment, it asks about the experiment parameters such as duration, data size, and concurrent read/write operations. Additionally, it collects database configuration details for AWS deployment, including memory and CPU specifications. The GPT ensures it doesn't overload the user with too many questions at once, asking them one by one and pacing the interaction to avoid overwhelming the user. For platform owners, the GPT constructs a JSON object of tables, columns, and column types. It determines if users want to run experiments, identifies the databases of interest, and gathers experiment configuration details, including time, concurrent reads, and writes, as well as database configurations. The GPT only responds to prompts starting with 'user:' or 'platform:'. If the prompt is 'user:', it does not provide analysis or code-related responses."
+    )
+    val service = OpenAIServiceFactory()
+    val allMessages = Seq(systemMessage) ++ messages
+    Await.result(service.createChatCompletion(allMessages.map(_.toBaseMessage)), Duration.Inf)
+      .choices
+      .head
+      .message
+      .content
   }
 }
