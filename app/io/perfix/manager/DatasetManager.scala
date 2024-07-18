d2d49cda7b8cb21a2823ffd56ab21b8c66a50ab1
Mridul Verma <mridulv09@gmail.com>
2024-07-18 20:22:45 +0530

    Removing duplication in datasets

--- app/io/perfix/manager/DatasetManager.scala
+++ app/io/perfix/manager/DatasetManager.scala
@@ -48,7 +48,7 @@ class DatasetManager @Inject()(datasetConfigStore: DatasetConfigStore) {
             dataset.copy(tableName = Some(dataset.tableName.getOrElse(s"table_${index}")))
           }
       }.getOrElse(throw InvalidStateException("Invalid Dataset"))
-    Datasets(datasets ++ datasets)
+    Datasets(datasets)
   }
 
   def update(datasetId: DatasetId,
