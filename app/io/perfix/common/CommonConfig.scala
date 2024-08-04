2c70ca1c0cf4236f5e61f46c386321c87b2426d0
Mridul Verma <mridulv09@gmail.com>
2024-08-04 14:05:42 +0530

    Adding EKS_CLUSTER_NAME to env properties

--- app/io/perfix/common/CommonConfig.scala
+++ app/io/perfix/common/CommonConfig.scala
@@ -3,6 +3,7 @@ package io.perfix.common
 object CommonConfig {
 
   val DB_SUBNET_GROUP_NAME: String = Option(System.getenv("DB_SUBNET_GROUP_NAME")).map(_.toLowerCase()).getOrElse("dbsubnet")
+  val EKS_CLUSTER_NAME: String = Option(System.getenv("EKS_CLUSTER_NAME")).map(_.toLowerCase()).getOrElse("prod-cluster")
   val IS_TRIAL_MODE: Boolean = Option(System.getenv("MODE")).map(_.toLowerCase()) match {
     case Some("trial") => true
     case _ => false
