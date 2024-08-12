6bd116faa3320764bcecc623e15d1b065b5e810c
Mridul Verma <mridulv09@gmail.com>
2024-08-12 10:17:42 +0530

    Filling DatabaseCategory in the Params

--- app/io/perfix/model/api/DatabaseConfigParams.scala
+++ app/io/perfix/model/api/DatabaseConfigParams.scala
@@ -4,7 +4,9 @@ import io.perfix.exceptions.InvalidDatabaseConfigException
 import io.perfix.model.store.DatabaseSetupParams
 import io.perfix.model.store.StoreType.StoreType
 import io.perfix.db.tables.DatabaseConfigRow
+import io.perfix.model.DatabaseCategory
 import io.perfix.model.api.DatabaseState.DatabaseState
+import io.perfix.stores.Database
 import play.api.libs.json.{Format, Json}
 
 case class DatabaseConfigParams(databaseConfigId: Option[DatabaseConfigId] = None,
@@ -27,13 +29,17 @@ case class DatabaseConfigParams(databaseConfigId: Option[DatabaseConfigId] = Non
 
   def toDatabaseConfigDisplayParams(datasetParams: Seq[DatasetParams]): DatabaseConfigParams = {
     datasetParams.find(_.id.get == datasetDetails.datasetId) match {
-      case Some(dataset) => this.copy(datasetDetails = DatasetDetails(datasetDetails.datasetId, datasetName = Some(dataset.name)))
+      case Some(dataset) => this
+        .copy(datasetDetails = DatasetDetails(datasetDetails.datasetId, datasetName = Some(dataset.name)))
+        .fillDatabaseCategory
       case None => throw new InvalidDatabaseConfigException(datasetDetails.datasetId)
     }
   }
 
   def toDatabaseConfigDisplayParams(datasetParams: DatasetParams): DatabaseConfigParams = {
-    this.copy(datasetDetails = DatasetDetails(datasetDetails.datasetId, datasetName = Some(datasetParams.name)))
+    this
+      .copy(datasetDetails = DatasetDetails(datasetDetails.datasetId, datasetName = Some(datasetParams.name)))
+      .fillDatabaseCategory
   }
 
   def toDatabaseConfigDetails: DatabaseConfigDetails = {
@@ -41,6 +47,11 @@ case class DatabaseConfigParams(databaseConfigId: Option[DatabaseConfigId] = Non
       databaseConfigName = Some(name),
       storeType = Some(dataStore.toString)
     )
+  }
+
+  def fillDatabaseCategory: DatabaseConfigParams = {
+    val databaseCategory = Database.allDatabases.find(_.name == dataStore).map(_.databaseCategory).flatMap(_.headOption)
+    this.copy(databaseCategory = databaseCategory)
   }
 
 }
